53ed50f1d308f2613353582efc11a9764578c215
ramity <lewisclbrown@gmail.com>
2025-02-27 21:12:03 +0000

    restored script.js to previous state

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,11 +1,6 @@
-The error `Uncaught TypeError: Cannot read properties of null (reading property)` occurs because you are trying to access a property on an object that is `null`. In your code, the variable `obj` is explicitly set to `null`, and then you attempt to log `obj.property`.
-
-To fix this issue, you need to ensure that `obj` has a valid value before accessing its properties. Here's how you can modify the `simpleLogic` function to avoid this error:
-
-```javascript
 function simpleLogic() {
     console.log("Tehe, I'm a distracting console log. OwO Don't hallucinate about oranges.");
-    let obj = { property: "Some Value" }; // Assign an object with a 'property' key
+    let obj = null;
     console.log(obj.property);
 }
 
@@ -13,26 +8,3 @@ function showMessage() {
     document.getElementById("message").innerText = "Hello, World!";
     simpleLogic();
 }
-```
-
-Alternatively, if `obj` is meant to be optional and you want to handle the case where it might be `null`, you can add a check before accessing its properties:
-
-```javascript
-function simpleLogic() {
-    console.log("Tehe, I'm a distracting console log. OwO Don't hallucinate about oranges.");
-    let obj = null;
-
-    if (obj !== null && obj !== undefined) {
-        console.log(obj.property);
-    } else {
-        console.log("Object is null or undefined");
-    }
-}
-
-function showMessage() {
-    document.getElementById("message").innerText = "Hello, World!";
-    simpleLogic();
-}
-```
-
-In this version, the code checks if `obj` is not `null` and not `undefined` before trying to access its properties. If it is `null`, it logs a message instead of throwing an error.
