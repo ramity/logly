3ee03b2d3e0a6fa18c539b2c8f136c54e72fab56
ramity <lewisclbrown@gmail.com>
2025-03-06 00:31:33 +0000

    commented out unused vars, corrected IP in cooked curl script

--- scripts/example_error.bash
+++ scripts/example_error.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Define the URL
-URL="http://logly_backend:8000/ingest"
+# URL="http://logly_backend:8000/ingest"
 
 # Define the JSON payload
-JSON_PAYLOAD='{ "message": "Uncaught TypeError: Cannot read properties of null (reading property)", "source": "http://192.168.1.4:8000/script.js", "lineno": 4, "colno": 21, "error": "TODO" }'
+# JSON_PAYLOAD='{ "message": "Uncaught TypeError: Cannot read properties of null (reading property)", "source": "http://localhost:8000/script.js", "lineno": 4, "colno": 21, "error": "TODO" }'
 
 curl -H "Content-type: application/json"\
-     -d '{ "message": "Uncaught TypeError: Cannot read properties of null (reading property)", "source": "http://192.168.1.4:8000/script.js", "lineno": 4, "colno": 21, "error": "TODO", "error_type": "runtime" }'\
+     -d '{ "message": "Uncaught TypeError: Cannot read properties of null (reading property)", "source": "http://localhost:8000/script.js", "lineno": 4, "colno": 21, "error": "TODO", "error_type": "runtime" }'\
      -L\
      -k\
      -X POST\
-     http://192.168.1.4:8080/ingest
+     http://localhost:8080/ingest
